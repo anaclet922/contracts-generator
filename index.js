19c424082194e2f391e22727d90041b4e9a01725
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2024-01-31 16:42:13 +0200

    lenovo - 31012024

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const compression = require("compression");
+var DocxMerger = require('docx-merger');
+
 const app = express()
 const port = 3000
 
+
+app.use(compression()); // Compress all routes
 //to use public files; css, js, images...inside public folder
-app.use(express.static('public'))
+app.use(express.static('public'));
+// app.use(express.static('contracts'));
 app.set('view engine', 'ejs');
 
 // app.use(bodyParser.json()) // for parsing application/json
@@ -20,6 +26,9 @@ const fs = require("fs");
 const path = require("path");
 
 
+app.use(express.static(path.join(__dirname, 'contracts')));
+app.use(express.static(path.join(__dirname, 'proposals')));
+app.use(express.static(path.join(__dirname, 'tmp')));
 
 const fss = require('fs').promises;
 const libre = require('libreoffice-convert');
@@ -34,16 +43,14 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.status(200).json({ message: "API test endpoint reached" });
 });
+
 app.post('/generate-contract', async function (req, res) {
 
-    let names = req.body.names;
-    let nid = req.body.nid;
-    let policy = req.body.policy;
-
-
+    let policy = req.body.police;
     let type = req.body.type;
     let lang = req.body.lang;
 
+    let all_body = req.body;
 
     // Load the docx file as binary content
     let content = fs.readFileSync(
@@ -51,24 +58,34 @@ app.post('/generate-contract', async function (req, res) {
         "binary"
     );
 
-    if (type == 'porc') {
+    if (type == 'crop') {
         content = fs.readFileSync(
-            path.resolve(__dirname, "/docx-templates/template-porc-" + lang + ".docx"),
+            path.resolve(__dirname, "docx-templates/template-crop-" + lang + ".docx"),
             "binary"
         );
-    } else if (type == 'crop') {
+    } else if (type == 'chicken') {
         content = fs.readFileSync(
-            path.resolve(__dirname, "/docx-templates/template-porc-" + lang + ".docx"),
+            path.resolve(__dirname, "docx-templates/template-chicken-" + lang + ".docx"),
+            "binary"
+        );
+    } else if (type == 'pig') {
+        content = fs.readFileSync(
+            path.resolve(__dirname, "docx-templates/template-pig-" + lang + ".docx"),
+            "binary"
+        );
+    } else if (type == 'cow') {
+        content = fs.readFileSync(
+            path.resolve(__dirname, "docx-templates/template-cow-" + lang + ".docx"),
+            "binary"
+        );
+    } else if (type == 'student') {
+        content = fs.readFileSync(
+            path.resolve(__dirname, "docx-templates/template-student-" + lang + ".docx"),
             "binary"
         );
     } else if (type == 'accident') {
         content = fs.readFileSync(
             path.resolve(__dirname, "/docx-templates/template-accident-" + lang + ".docx"),
-            "binary"
-        );
-    } else if (type == 'school') {
-        content = fs.readFileSync(
-            path.resolve(__dirname, "/docx-templates/template-school-" + lang + ".docx"),
             "binary"
         );
     }
@@ -80,12 +97,7 @@ app.post('/generate-contract', async function (req, res) {
         linebreaks: true,
     });
     // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
-    doc.render({
-        names: names,
-        nid: nid,
-        policy: policy
-
-    });
+    doc.render(all_body);
 
     const buf = doc.getZip().generate({
         type: "nodebuffer",
@@ -121,10 +133,193 @@ app.post('/generate-contract', async function (req, res) {
 
 
     let output = {
-        filename: filename_pdf
+        filename: filename_pdf + ext
     }
     res.status(200).json(output);
 
 });
+
+app.post('/generate-proposal', async function (req, res) {
+
+
+    let nsin = req.body.nsin;
+    let typpol = req.body.typpol;
+    let all_body = req.body;
+
+    // Load the docx file as binary content
+    let content = fs.readFileSync(
+        path.resolve(__dirname, "docx-templates/template-payment-proposal-" + typpol + ".docx"),
+        "binary"
+    );
+
+
+
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+    });
+    // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+    doc.render(all_body);
+
+    const buf = doc.getZip().generate({
+        type: "nodebuffer",
+        // compression: DEFLATE adds a compression step.
+        // For a 50MB output document, expect 500ms additional CPU time
+        compression: "DEFLATE",
+    });
+    // buf is a nodejs Buffer, you can either write it to a
+    // file or res.send it with express for example.
+    let filename = nsin + "-output.docx";
+    fs.writeFileSync(path.resolve(__dirname, "proposals/" + filename), buf);
+
+
+
+
+    let output = {
+        filename: filename
+    }
+    res.status(200).json(output);
+
+});
+
+app.get('/download/:filename', (req, res) => {
+
+    const filePath = __dirname + '/contracts/' + req.params.filename;
+
+    res.download(
+        filePath,
+        "downloaded-" + req.params.filename,
+        (err) => {
+            if (err) {
+                res.send({
+                    error: err,
+                    msg: "Problem downloading the file"
+                })
+            }
+        }
+    );
+
+});
+
+app.get('/download-proposal/:filename', (req, res) => {
+
+    const filePath = __dirname + '/proposals/' + req.params.filename;
+
+    res.download(
+        filePath,
+        "downloaded-" + req.params.filename,
+        (err) => {
+            if (err) {
+                res.send({
+                    error: err,
+                    msg: "Problem downloading the file"
+                })
+            }
+        }
+    );
+
+});
+
+app.get('/delete-old-files', (req, res) => {
+
+    const directory = "contracts";
+    const tmp = "tmp";
+
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), (err) => {
+                if (err) throw err;
+            });
+        }
+    });
+
+    fs.readdir(tmp, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(tmp, file), (err) => {
+                if (err) throw err;
+            });
+        }
+    });
+
+    let output = {
+        response: 'cleaning'
+    }
+    res.status(200).json(output);
+
+});
+
+
+
+app.post('/generate-combined-proposal', async function (req, res) {
+
+    try {
+
+        let folder_name = req.body.folder_name;
+        let files = req.body.files;
+
+        console.log(files);
+
+        // Array to hold file buffers
+        let fileBuffers = [];
+
+        // Read each file asynchronously
+        for (let file of files) {
+            let filePath = path.resolve(__dirname, "proposals/" + file);
+            let fileBuffer = fs.readFileSync(filePath, 'binary');
+            fileBuffers.push(fileBuffer);
+        }
+
+        var docx = new DocxMerger({}, fileBuffers);
+
+        let filename = folder_name + '.docx';
+
+        //SAVING THE DOCX FILE
+        docx.save('nodebuffer', function (data) {
+            fs.writeFile(path.resolve(__dirname, "tmp/" + filename), data, function (err) {
+                if (err) {
+                    console.error("Error saving file:", err);
+                    return res.status(500).json({ error: "Error saving file" });
+                }
+                let output = {
+                    filename: filename
+                }
+                return res.status(200).json(output);
+            });
+        });
+
+
+    } catch (error) {
+
+        console.error("Error generating combined proposal:", error);
+        return res.status(500).json({ error: "Error generating combined proposal" });
+
+    }
+});
+
+
+app.get('/download-combined-proposal/:filename', (req, res) => {
+
+    const filePath = __dirname + '/tmp/' + req.params.filename;
+
+    res.download(
+        filePath,
+        "downloaded-" + req.params.filename,
+        (err) => {
+            if (err) {
+                res.send({
+                    error: err,
+                    msg: "Problem downloading the file"
+                })
+            }
+        }
+    );
+
+});
+
 
 app.listen(port, () => { console.log('App Started at port ' + port) });
